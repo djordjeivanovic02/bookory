bb161af2bdc2890a483cc05936492c42031e3e39
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-07-14 21:37:36 +0200

    Napravljen dizajn search-a

--- src/app/screens/navi/navi.component.ts
+++ src/app/screens/navi/navi.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { faSearch, faUser, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-navi',
-  templateUrl: './navi.component.html',
-  styleUrl: './navi.component.scss'
+  selector: "app-navi",
+  templateUrl: "./navi.component.html",
+  styleUrl: "./navi.component.scss"
 })
 export class NaviComponent {
-
+  faSearch = faSearch;
+  faUser = faUser;
+  faHeart = faHeart;
 }
