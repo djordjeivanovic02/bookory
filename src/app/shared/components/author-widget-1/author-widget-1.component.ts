06e4de772d0d46722cfaccb53cb6ac196c2af725
Djordje Ivanovic <djordjeivanovic@elfak.rs>
2024-07-17 15:13:37 +0200

    Napravljen prikaz najboljih autora

--- src/app/shared/components/author-widget-1/author-widget-1.component.ts
+++ src/app/shared/components/author-widget-1/author-widget-1.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { faHeart as faRegularHeart } from "@fortawesome/free-regular-svg-icons";
 
 @Component({
-  selector: 'app-author-widget-1',
-  templateUrl: './author-widget-1.component.html',
-  styleUrl: './author-widget-1.component.scss'
+  selector: "app-author-widget-1",
+  templateUrl: "./author-widget-1.component.html",
+  styleUrl: "./author-widget-1.component.scss",
 })
 export class AuthorWidget1Component {
-
+  faStar = faStar;
+  faHeart = faRegularHeart;
+  stars = Array(5)
+    .fill(0)
+    .map((x, i) => i);
 }
