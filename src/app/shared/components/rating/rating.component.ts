d099ccf2e6ac2cd815056f1e4223eb1fde044e9c
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-07-21 22:50:06 +0200

    Napravljeno dodavanje korisnikove recenzije

--- src/app/shared/components/rating/rating.component.ts
+++ src/app/shared/components/rating/rating.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-rating',
-  templateUrl: './rating.component.html',
-  styleUrl: './rating.component.scss'
+  selector: "app-rating",
+  templateUrl: "./rating.component.html",
+  styleUrl: "./rating.component.scss",
 })
 export class RatingComponent {
+  maxStars = 5;
+  rating = 0;
+  isRatingSet = false;
+  hoveredStar: number | null = null;
 
+  setRating(star: number) {
+    this.hoveredStar = null;
+    this.rating = star;
+    this.isRatingSet = true;
+  }
+
+  isStarSelected(star: number) {
+    return this.rating >= star;
+  }
+
+  isStarHovered(star: number) {
+    return this.hoveredStar !== null && star <= this.hoveredStar;
+  }
+
+  hoverStar(star: number) {
+    if (!this.isRatingSet) {
+      this.hoveredStar = star;
+    }
+  }
+
+  unhoverStar() {
+    if (!this.isRatingSet) {
+      this.hoveredStar = null;
+    }
+  }
 }
