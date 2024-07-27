907e21716809c751bf95d9268da97d5462d04ccc
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-07-27 13:22:44 +0200

    Napravljen prikaz autorovih knjiga

--- src/app/shared/components/book-widget-3/book-widget-3.component.ts
+++ src/app/shared/components/book-widget-3/book-widget-3.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  faFacebookF,
+  faInstagram,
+  faLinkedinIn,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEdit } from "@fortawesome/free-regular-svg-icons";
+import { faDeleteLeft, faRemove } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
-  selector: 'app-book-widget-3',
-  templateUrl: './book-widget-3.component.html',
-  styleUrl: './book-widget-3.component.scss'
+  selector: "app-book-widget-3",
+  templateUrl: "./book-widget-3.component.html",
+  styleUrl: "./book-widget-3.component.scss",
 })
 export class BookWidget3Component {
-
+  faRemove = faRemove;
+  faEdit = faEdit;
 }
