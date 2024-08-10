261816f33baab3fb31113d7486883c61fb23e1e7
Djordje Ivanovic <djordjeivanovic05@hotmail.com>
2024-08-10 17:18:34 +0200

    Napravljen store za najnovije knjige

--- frontend/src/app/shared/components/book-widget-1/book-widget-1.component.ts
+++ frontend/src/app/shared/components/book-widget-1/book-widget-1.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as faRegularHeart } from "@fortawesome/free-regular-svg-icons";
+import { BookInfoDto } from "../../dtos/book-info.dto";
 @Component({
   selector: "app-book-widget-1",
   templateUrl: "./book-widget-1.component.html",
@@ -12,4 +13,7 @@ export class BookWidget1Component {
   stars = Array(5)
     .fill(0)
     .map((x, i) => i);
+
+  @Input()
+  book: BookInfoDto | null = null;
 }
